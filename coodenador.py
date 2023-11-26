7124405a43c4a0173c2c204c4b24b9cfd18b82a9
IssamuTatch <issamu.oba.matsumoto@gmail.com>
2023-11-26 14:25:13 -0300

    Mudança das mensagens e adição da interface.

--- coodenador.py
+++ coodenador.py
@@ -1,31 +1,40 @@
 import socket
 import threading
+import time
+import sys
 
 waiting_list = []
 lock = threading.Lock()
-
+identificador_count = {}
 
 def coordenador(c, addr):
     while True:
         with lock:
             decoded_data = c.recv(1024).decode('ascii')
 
-            if decoded_data == 'REQUEST':
-                print('REQUEST')
+            if decoded_data.startswith('1|') and decoded_data.endswith('|00') and decoded_data[2:7].isdigit():
+                if identificador_count.get(decoded_data[2:7]):
+                    pass
+                else:
+                    identificador_count[decoded_data[2:7]] = 0
+                #print(decoded_data)
                 waiting_list.append((c, addr))
 
                 if waiting_list[0] == (c, addr):
                     next_c, next_addr = waiting_list[0]
-                    print('GRANT')
-                    next_c.send('GRANT'.encode('ascii'))
+                    grant_mensage = '2|'+str(decoded_data[2:7])+'|00'
+                    #print(grant_mensage)
+                    next_c.send(grant_mensage.encode('ascii'))
+                    identificador_count[decoded_data[2:7]] = identificador_count.get(decoded_data[2:7])+1
+                    #print(decoded_data)
 
-            if decoded_data == 'RELEASE':
-                print('RELEASE')
+            if decoded_data.startswith('3|') and decoded_data.endswith('|00') and decoded_data[2:7].isdigit():
+                #print(decoded_data)
                 waiting_list.pop(0)
 
                 if waiting_list:
                     next_c, next_addr = waiting_list[0]
-                    next_c.send('GRANT'.encode('ascii'))
+                    next_c.send(('2|'+str(decoded_data[2:7])+'|00').encode('ascii'))
 
 
 def main():
@@ -41,9 +50,20 @@ def main():
 
     while True:
         c, addr = s.accept()
-
-        threading.Thread(target=coordenador, args=(c, addr)).start()
+        threading.Thread(target=coordenador, args=(c, addr), daemon=True).start()
 
 
 if __name__ == '__main__':
-    threading.Thread(target=main).start()
+    threading.Thread(target=main, daemon=True).start()
+    time.sleep(1)
+    print('1 - Imprimir a fila de pedidos atual')
+    print('2 - Imprimir quantas vezescada processo foi atendido')
+    print('3 - Encerrar a execução')
+    while True:
+        command = input('>> ')
+        if command.lower() == '1':
+            print(waiting_list)
+        elif command.lower() == '2':
+            print(identificador_count)
+        elif command.lower() == '3':
+            sys.exit("Finalizando o programa")
--- processo.py
+++ processo.py
@@ -15,25 +15,25 @@ def processo():
 
     print('Connected to Server')
 
-    for _ in range(3):
-        s.send('REQUEST'.encode('ascii'))
-
+    for _ in range(5):
+        processo_port = s.getsockname()[1]
+        s.send(('1|'+str(processo_port)+'|00').encode('ascii'))
         grant_signal = s.recv(1024).decode('ascii')
 
-        if grant_signal == 'GRANT':
+        if grant_signal.startswith('2|') and grant_signal.endswith('|00') and grant_signal[2:7].isdigit():
             with lock:
                 with open('resultado.txt', 'a') as file:
                     now = str(datetime.datetime.now())
-                    file.write(now + '|' + str(s.getsockname()) + '\n')
-
-            s.send('RELEASE'.encode('ascii'))
+                    file.write(str(s.getsockname()) + '|' + now  +  '\n')
+                time.sleep(1)
+            s.send(('3|'+str(processo_port)+'|00').encode('ascii'))
             time.sleep(1)
 
     s.close()
 
 
 def main():
-    for _ in range(3):
+    for _ in range(5):
         threading.Thread(target=processo).start()
 
 
